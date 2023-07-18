431e0563afd92fc502b6adb45e6882fe585de766
Denis Latyshau <denislatyshau@yandex.ru>
2023-07-18 16:03:45 +0300

    feat: add context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import { Product } from './components/Product';
 import { useProducts } from './hooks/products';
 import { Loader } from './components/Loader';
@@ -6,13 +6,14 @@ import { ErrorMessage } from './components/ErrorMessage';
 import { Modal } from './components/Modal';
 import { CreateProduct } from './components/CreateProduct';
 import { IProduct } from './models';
+import { ModalContext } from './context/ModalContext';
 
 function App() {
   const {products, loading, error, addProduct} = useProducts()
-  const [modal, setModal] = useState(false)
+  const {modal, open, close} = useContext(ModalContext)
 
   const createHandler = (product: IProduct) => {
-    setModal(false)
+    close()
     addProduct(product)
   }
 
@@ -22,13 +23,13 @@ function App() {
       { error &&  <ErrorMessage error={error} /> }
       { products.map(product => <Product product={product} key={product.id}/>) }
 
-      { modal && <Modal title='Create new product' onClose={() => setModal(false)}>
+      { modal && <Modal title='Create new product' onClose={close}>
         <CreateProduct onCreate={createHandler}/>
       </Modal> }
 
       <button 
         className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
-        onClick={() => setModal(true)}
+        onClick={open}
       >+</button>
     </div>
   )
